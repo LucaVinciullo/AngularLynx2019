8621fae36873571dfa731b06780074deb1c06e7f
LucaVinciullo <vnclcu@hotmail.it>
2019-04-03 17:56:51 +0200

    Feedback Form Template implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './components/list/list.component';
@@ -18,6 +18,7 @@ import { FeedbackComponent } from './components/feedback/feedback.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
