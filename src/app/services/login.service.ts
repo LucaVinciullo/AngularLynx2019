e890083c010012f72233cdccec4a77d86f3c0087
LucaVinciullo <vnclcu@hotmail.it>
2019-04-10 15:55:06 +0200

    style upgrade

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  authentication : boolean = false;
+  private loginService = new Subject<string>();
+  loginService$ = this.loginService.asObservable();
+  
+  loginEvent() {
+    this.loginService.next();
+  }
 
-  constructor() { }
+  getAuthentication(): boolean {
+    return this.authentication;
+  }
+
+  Authenticate(username: string, password: string): void {
+    (!username || username != '') ? localStorage.getItem(username) : this.authentication= false;
+  }
+
+  authenticationTrue(username: string): void {
+    this.authentication= true;
+    sessionStorage.setItem('username', username);
+  }
+  constructor() {   
+    // user base set  up
+    localStorage.setItem('username', 'password');
+    localStorage.setItem('Borgogno', 'Borgogno');
+    localStorage.setItem('El Bassri', 'El Bassri');
+  }
 }
